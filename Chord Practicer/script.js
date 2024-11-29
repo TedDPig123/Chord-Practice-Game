341fb0171c29473bdf08308eabaca72ac839843a
TedDPig_git <85619008+TedDPig123@users.noreply.github.com>
2024-11-29 16:40:32 -0500

    added timer functionality

--- Chord Practicer/script.js
+++ Chord Practicer/script.js
@@ -39,6 +39,12 @@ function displayPlayMenu(){
     const startMenu = document.querySelector('.start-menu');
     playMenu.style.display = 'flex';
     startMenu.style.display = 'none';
+
+    const minutes = document.getElementById('input-minutes').value;
+    const seconds = document.getElementById('input-seconds').value;
+
+    const duration = (parseInt(minutes)*60) + parseInt(seconds);
+    countdownStart(duration);
 }
 
 function displayStartMenu(){
@@ -50,6 +56,26 @@ function displayStartMenu(){
 
 startButton.addEventListener('click', displayPlayMenu);
 quitButton.addEventListener('click', displayStartMenu);
+
+function countdownStart(timer){
+    let duration = timer;
+    let minutes; 
+    let seconds;
+    let display = document.getElementById('curr-time');
+    const interval = setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            clearInterval(interval);
+        }
+    }, 1000);
+}
 
 // window.addEventListener('DOMContentLoaded', function() {
 // });
